72a72332b1d9dcb6afb0e2cb72f13e9408c44181
TerukiToyonaga <127729072+teru5678@users.noreply.github.com>
2023-04-29 06:06:37 +0000

    [Add]フォロー/フォロワー一覧コントローラー

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @posts = @user.posts.page(params[:page]).reverse_order
+    @following_users = @user.following_user
+    @follower_users = @user.folower_user
     @books = @user.books
     @book = Book.new
   end
@@ -23,7 +26,17 @@ class UsersController < ApplicationController
       render "edit"
     end
   end
-
+  
+  def follows
+    user = User.find(params[:id])
+    @users = user.following_user.page(params[:page]).per(3).reverse_order
+  end
+  
+  def followers
+    user = User.fimd(params[:id])
+    @users = user.follower_user.page(params[:page]).per(3).reverse_order
+  end
+  
   private
 
   def user_params
