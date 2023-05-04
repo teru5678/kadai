3a45470cb0800d24c4d76bf2466f2119253a2002
TerukiToyonaga <127729072+teru5678@users.noreply.github.com>
2023-05-04 03:33:04 +0000

    [Finish]応用課題５

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,9 +28,24 @@ end
 
 def following?(user)
   following_user.include?(user)
-end	 
+end
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+
+#検索方法分岐
+  def self.looks(search,word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?", "#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?", "%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- 
+
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
   devise_for :users
+  get "search" => "searches#search"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :post_comments, only: [:create,:destroy]
