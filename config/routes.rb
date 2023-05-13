ab58baf97da1ba54e5ba62d8076a418fc30ebef9
TerukiToyonaga <127729072+teru5678@users.noreply.github.com>
2023-05-13 06:37:16 +0000

    [Add]chatsコントローラー記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'chats/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root :to =>"homes#top"
@@ -15,5 +16,7 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#follows', as: 'follows'
   	get 'followers' => 'relationships#followers', as: 'followers'
   end
+  
+  resources :chats, only: [:show,:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
